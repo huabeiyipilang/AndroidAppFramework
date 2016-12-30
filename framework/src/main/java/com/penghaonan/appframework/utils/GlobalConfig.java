1510768481787f41a40447b7cfce768392db2e9e
huabeiyipilang <huabeiyipilang@gmail.com>
2016-12-30 15:42:38 +0800

    重构GlobalConfig

--- framework/src/main/java/com/penghaonan/appframework/utils/GlobalConfig.java
+++ framework/src/main/java/com/penghaonan/appframework/utils/GlobalConfig.java
@@ -6,24 +6,29 @@ import android.content.SharedPreferences;
 import com.penghaonan.appframework.AppDelegate;
 
 public class GlobalConfig {
-    private static GlobalConfig sInstance;
 
-    private Context mContext;
-    private SharedPreferences mPref;
+    private static SharedPreferences sPref = AppDelegate.getApp().getSharedPreferences(AppDelegate.getApp().getPackageName(), Context.MODE_PRIVATE);
 
-    private GlobalConfig(Context context) {
-        mContext = context;
-        mPref = mContext.getSharedPreferences(mContext.getPackageName(), Context.MODE_PRIVATE);
+    private GlobalConfig() {
     }
 
-    public static GlobalConfig getInstance() {
-        if (sInstance == null) {
-            sInstance = new GlobalConfig(AppDelegate.getApp());
-        }
-        return sInstance;
+    public static SharedPreferences getDefault() {
+        return sPref;
     }
 
-    public SharedPreferences getDefault() {
-        return mPref;
+    public static void put(String key, boolean value) {
+        sPref.edit().putBoolean(key, value).apply();
+    }
+
+    public static boolean getBoolean(String key, boolean def) {
+        return sPref.getBoolean(key, def);
+    }
+
+    public static void put(String key, String value) {
+        sPref.edit().putString(key, value).apply();
+    }
+
+    public static String getString(String key, String def) {
+        return sPref.getString(key, def);
     }
 }
