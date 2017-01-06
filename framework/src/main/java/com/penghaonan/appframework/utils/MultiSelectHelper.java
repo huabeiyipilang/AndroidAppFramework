0b19486e57e149621670e7bbdfd3903fc4bdbc7e
huabeiyipilang <huabeiyipilang@gmail.com>
2017-01-06 19:15:37 +0800

    多选Helper

---
+++ framework/src/main/java/com/penghaonan/appframework/utils/MultiSelectHelper.java
@@ -0,0 +1,45 @@
+package com.penghaonan.appframework.utils;
+
+import android.support.annotation.NonNull;
+import android.util.SparseBooleanArray;
+
+import java.util.Collection;
+
+public class MultiSelectHelper {
+    private SparseBooleanArray checkedArray;
+
+    public MultiSelectHelper() {
+        checkedArray = new SparseBooleanArray();
+    }
+
+    public void setChecked(int position, boolean checked) {
+        checkedArray.put(position, checked);
+    }
+
+    public void toggleChecked(int position) {
+        setChecked(position, !isChecked(position));
+    }
+
+    public boolean isChecked(int position) {
+        return checkedArray.get(position, false);
+    }
+
+    public void iterator(Collection data, @NonNull Iterator iterator) {
+
+        java.util.Iterator iterator1 = data.iterator();
+        int i = 0;
+        while (iterator1.hasNext()) {
+            if (isChecked(i)) {
+                iterator.onNext(iterator1.next());
+            }
+        }
+    }
+
+    public void clear() {
+        checkedArray.clear();
+    }
+
+    public interface Iterator<T> {
+        void onNext(T t);
+    }
+}
