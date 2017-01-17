1ec7980b5d5496cbf251facd549f98ac04dcdb76
Carl Li <huabeiyipilang@gmail.com>
2017-01-17 16:03:36 +0800

    设置Activity状态栏和导航栏颜色

--- framework/src/main/java/com/penghaonan/appframework/utils/UiUtils.java
+++ framework/src/main/java/com/penghaonan/appframework/utils/UiUtils.java
@@ -2,7 +2,16 @@ package com.penghaonan.appframework.utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.view.ViewCompat;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.penghaonan.appframework.AppDelegate;
 
 /**
  * Created by carl on 9/16/15.
@@ -28,5 +37,36 @@ public class UiUtils {
         float density = metric.density;      // 屏幕密度（0.75 / 1.0 / 1.5）
         int densityDpi = metric.densityDpi;  // 屏幕密度DPI（120 / 160 / 240）
         return 0;
+    }
+
+    /**
+     * 设置StatusBar颜色
+     */
+    public static void initActivityStatusNavigationBarColor(Activity activity, int color) {
+        initActivityStatusNavigationBarColor(activity,
+                AppDelegate.getApp().getResources().getColor(color),
+                Color.BLACK);
+    }
+
+    /**
+     * 设置StatusBar， NavigationBar颜色
+     */
+    public static void initActivityStatusNavigationBarColor(Activity activity, int sta_color, int nav_color) {
+        if (activity != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = activity.getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            if (sta_color != 0) {
+                window.setStatusBarColor(sta_color);
+            }
+            if (nav_color != 0) {
+                window.setNavigationBarColor(nav_color);
+            }
+            ViewGroup mContentView = (ViewGroup) activity.findViewById(Window.ID_ANDROID_CONTENT);
+            View mChildView = mContentView.getChildAt(0);
+            if (mChildView != null) {
+                ViewCompat.setFitsSystemWindows(mChildView, true);
+            }
+        }
     }
 }
