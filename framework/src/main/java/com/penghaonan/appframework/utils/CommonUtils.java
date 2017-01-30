5d7cb5b1221a755d0e7d8279395beb14a3a53628
Carl Li <huabeiyipilang@gmail.com>
2017-01-30 11:58:39 +0800

    增加主进程判断接口

--- framework/src/main/java/com/penghaonan/appframework/utils/CommonUtils.java
+++ framework/src/main/java/com/penghaonan/appframework/utils/CommonUtils.java
@@ -1,13 +1,21 @@
 package com.penghaonan.appframework.utils;
 
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
+
+import com.penghaonan.appframework.AppDelegate;
 
 import java.util.Iterator;
 import java.util.List;
 
 public class CommonUtils {
+
+    /**
+     * 检查需要的权限
+     */
     public static void checkPermission(Activity activity, List<String> permissions) {
         if (activity == null || CollectionUtils.isEmpty(permissions)) {
             return;
@@ -20,7 +28,33 @@ public class CommonUtils {
             }
         }
         if (!CollectionUtils.isEmpty(permissions)) {
-            ActivityCompat.requestPermissions(activity, permissions.toArray(new String[0]),1);
+            ActivityCompat.requestPermissions(activity, permissions.toArray(new String[0]), 1);
         }
+    }
+
+    /**
+     * 是否是主进程
+     */
+    public static boolean isMainProcess() {
+        int pid = android.os.Process.myPid();
+        String processAppName = getProcessName(pid);
+        return processAppName != null && processAppName.equalsIgnoreCase(AppDelegate.getApp().getPackageName());
+    }
+
+    private static String getProcessName(int pID) {
+        String processName;
+        ActivityManager am = (ActivityManager) AppDelegate.getApp().getSystemService(Context.ACTIVITY_SERVICE);
+        List l = am.getRunningAppProcesses();
+        for (Object aL : l) {
+            ActivityManager.RunningAppProcessInfo info = (ActivityManager.RunningAppProcessInfo) (aL);
+            try {
+                if (info.pid == pID) {
+                    processName = info.processName;
+                    return processName;
+                }
+            } catch (Exception ignored) {
+            }
+        }
+        return null;
     }
 }
