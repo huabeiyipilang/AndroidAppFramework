f2860cff6465d94b898e93ac3df1fec061f33564
Carl Li <huabeiyipilang@gmail.com>
2017-03-06 20:54:35 +0800

    StorageUtils

--- framework/src/main/java/com/penghaonan/appframework/utils/StorageUtils.java
+++ framework/src/main/java/com/penghaonan/appframework/utils/StorageUtils.java
@@ -7,10 +7,17 @@ import java.io.File;
 public class StorageUtils {
 
     //应用外部文件夹名
-    private final static String DIR_NAME = "CLAppBackup";
+    private static String DIR_NAME;
+
+    public static void setRootDir(String dir) {
+        DIR_NAME = dir;
+    }
 
     //应用外部目录路径
     public static File getExternalFolder() {
+        if (DIR_NAME == null) {
+            throw new NullPointerException("setRootDir before use!");
+        }
         File dir = Environment.getExternalStorageDirectory();
         dir = new File(dir, DIR_NAME);
         if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
