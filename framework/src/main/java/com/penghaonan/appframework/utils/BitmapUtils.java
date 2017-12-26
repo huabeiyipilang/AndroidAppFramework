b3c0484806fe9b01998c401b1e9abcbb23ef2aad
Carl Li <huabeiyipilang@gmail.com>
2017-12-26 19:56:33 +0800

    BitmapUtils

--- framework/src/main/java/com/penghaonan/appframework/utils/BitmapUtils.java
+++ framework/src/main/java/com/penghaonan/appframework/utils/BitmapUtils.java
@@ -1,12 +1,17 @@
 package com.penghaonan.appframework.utils;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+
+import com.penghaonan.appframework.AppDelegate;
 
 public class BitmapUtils {
     public static Bitmap rotate(Bitmap bm, final int orientationDegree, Bitmap.Config config) {
@@ -58,5 +63,74 @@ public class BitmapUtils {
             drawable.draw(canvas);
             return bitmap;
         }
+    }
+
+
+    public static Bitmap getRatioBitmap(int imgId, int reqWidth, int reqHeight) {
+        BitmapFactory.Options newOpts = new BitmapFactory.Options();
+        newOpts.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(AppDelegate.getApp().getResources(), imgId, newOpts);
+        newOpts.inSampleSize = calculateInSampleSize(newOpts, reqWidth, reqHeight);
+
+        newOpts.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(AppDelegate.getApp().getResources(), imgId, newOpts);
+    }
+
+
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+        if (height > reqHeight || width > reqWidth) {
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+            while ((halfHeight / inSampleSize >= reqHeight) && (halfWidth / inSampleSize >= reqWidth)) {
+                inSampleSize *= 2;
+            }
+        }
+        return inSampleSize;
+    }
+
+    /**
+     * 转化成灰度图
+     */
+    public static Bitmap toGrayscale(Bitmap bmpOriginal) {
+
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height,
+                Bitmap.Config.RGB_565);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        ColorMatrix cm = new ColorMatrix();
+        cm.setSaturation(0);
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, 0, 0, paint);
+        return bmpGrayscale;
+    }
+
+    /**
+     * Bitmap压缩
+     */
+    public static Bitmap compress(final Bitmap bm, int reqWidth, int reqHeight) {
+        int width = bm.getWidth();
+        int height = bm.getHeight();
+
+        if (height > reqHeight || width > reqWidth) {
+            float scaleWidth = (float) reqWidth / width;
+            float scaleHeight = (float) reqHeight / height;
+            float scale = scaleWidth < scaleHeight ? scaleWidth : scaleHeight;
+
+            Matrix matrix = new Matrix();
+            matrix.postScale(scale, scale);
+            Bitmap result = Bitmap.createBitmap(bm, 0, 0, bm.getWidth(),
+                    bm.getHeight(), matrix, true);
+            bm.recycle();
+            return result;
+        }
+        return bm;
     }
 }
