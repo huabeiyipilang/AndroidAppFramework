cd14d9df421b62b76b5dd0351eee44d4a1eb3a87
Carl Li <huabeiyipilang@gmail.com>
2020-05-25 02:57:00 +0800

    not empty

--- framework/src/main/java/com/penghaonan/appframework/base/BaseFrameworkActivity.java
+++ framework/src/main/java/com/penghaonan/appframework/base/BaseFrameworkActivity.java
@@ -2,6 +2,19 @@ package com.penghaonan.appframework.base;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.penghaonan.appframework.reporter.Reporter;
+
 public class BaseFrameworkActivity extends AppCompatActivity {
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Reporter.getInstance().onActivityResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Reporter.getInstance().onActivityPause(this);
+    }
 }
--- framework/src/main/java/com/penghaonan/appframework/utils/CollectionUtils.java
+++ framework/src/main/java/com/penghaonan/appframework/utils/CollectionUtils.java
@@ -3,8 +3,21 @@ package com.penghaonan.appframework.utils;
 import java.util.Collection;
 
 public class CollectionUtils {
+
+    public static boolean isEmpty(Object[] array) {
+        return array == null || array.length == 0;
+    }
+
     public static boolean isEmpty(Collection collection) {
         return collection == null || collection.size() == 0;
+    }
+
+    public static boolean notEmpty(Collection collection) {
+        return !isEmpty(collection);
+    }
+
+    public static boolean notEmpty(Object[] array) {
+        return !isEmpty(array);
     }
 
     public static int size(Collection collection) {
