fe21717a741bfd77ab59801156008a3938b3d4c6
Carl Li <huabeiyipilang@gmail.com>
2020-08-02 14:13:50 +0800

    simple

--- framework/src/main/java/com/penghaonan/appframework/utils/wrapper/SimpleTextWatcher.java
+++ framework/src/main/java/com/penghaonan/appframework/utils/wrapper/SimpleTextWatcher.java
@@ -3,7 +3,7 @@ package com.penghaonan.appframework.utils.wrapper;
 import android.text.Editable;
 import android.text.TextWatcher;
 
-public class TextWatcherWrapper implements TextWatcher {
+public class SimpleTextWatcher implements TextWatcher {
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
